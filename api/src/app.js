0c0a2de0f76577d90f2981beb720a33c91d52744
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2023-05-25 19:14:08 +1200

    deploy fix

--- api/src/app.js
+++ api/src/app.js
@@ -5,41 +5,48 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors');
 
+
+
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
 
-const corsOptions = {
-  origin: 'https://spapokemonfront-production-d735.up.railway.app',
-  credentials: true,
-};
 
-server.use(cors(corsOptions));
+
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/uploads', express.static('./uploads'));
+
 server.use('/', routes);
 
-// Error catching middleware.
+
+
+// Error catching endware.
 server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
-
-server.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+server.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 module.exports = server;
-
 /*
 const multer = require('multer');
 const storage = multer.diskStorage({
