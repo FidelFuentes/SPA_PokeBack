a6029e35d1cd31d54c85a38b09805316dccc7635
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2023-05-25 20:02:56 +1200

    cors rebuild

--- api/src/app.js
+++ api/src/app.js
@@ -5,34 +5,29 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors');
 
-
-
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
 
-
-
-
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors()); // Use the cors middleware to handle CORS headers
+
+server.use('/uploads', express.static('./uploads'));
+
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-server.use('/uploads', express.static('./uploads'));
-
 server.use('/', routes);
-
-
 
 // Error catching endware.
 server.use((err, req, res, next) => {
@@ -41,12 +36,14 @@ server.use((err, req, res, next) => {
   console.error(err);
   res.status(status).send(message);
 });
+
 server.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 module.exports = server;
+
 /*
 const multer = require('multer');
 const storage = multer.diskStorage({
