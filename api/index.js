a32707ee31d276cd13abbc13c4a27d329dfcfcec
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2023-05-25 21:08:56 +1200

    fixing

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); // Importa la conexión a la base de datos definida en db.js
 
-const port =process.env.PORT_S || 3001
+const port = process.env.PORT || 3001; // Utiliza process.env.PORT en lugar de process.env.PORT_S
 
 // force true nos resetea nuestra base de datos.
 conn.sync({ force: true }).then(() => { // Sincroniza los modelos de la base de datos y luego inicia el servidor
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+  server.listen(port, '0.0.0.0', () => { // Escucha en la dirección '0.0.0.0' y utiliza el valor de process.env.PORT
+    console.log(`Server listening at ${port}`); // eslint-disable-line no-console
   });
 });
